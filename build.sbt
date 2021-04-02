798e7bd01c806908442dd95329be0a61f10c2cd7
Alexandru G Buzneanu <bznnalex@gmail.com>
2021-04-02 15:47:49 +0100

    Add server with API to get pokemon details & docs

--- build.sbt
+++ build.sbt
@@ -12,6 +12,12 @@ val circe = Seq(
   "io.circe" %% "circe-parser"
 ).map(_ % circeVersion)
 
+val logging = Seq(
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
+  "io.chrisdavenport" %% "log4cats-slf4j" % "1.1.1",
+  "net.logstash.logback" % "logstash-logback-encoder" % "6.4"
+)
+
 libraryDependencies ++= Seq(
   "org.typelevel" %% "cats-core" % "2.3.0",
   "org.typelevel" %% "cats-effect" % "2.3.0",
@@ -21,5 +27,8 @@ libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-circe" % "0.21.19",
   "com.softwaremill.sttp.tapir" %% "tapir-core" % "0.17.12",
   "com.softwaremill.sttp.tapir" %% "tapir-http4s-server" % "0.17.12",
-  "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % "0.17.12"
-) ++ circe
+  "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % "0.17.12",
+  "com.softwaremill.sttp.tapir" %% "tapir-openapi-docs" % "0.17.12",
+  "com.softwaremill.sttp.tapir" %% "tapir-openapi-circe-yaml" % "0.17.12",
+  "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-http4s" % "0.17.12",
+) ++ circe ++ logging
--- src/main/scala/org/pokemon/Server.scala
+++ src/main/scala/org/pokemon/Server.scala
@@ -1,22 +1,28 @@
 package org.pokemon
 
+import cats.data.EitherT
 import cats.effect.{ExitCode, IO, IOApp, Sync}
-import sttp.tapir._
-import sttp.tapir.server.http4s.Http4sServerInterpreter
-import io.circe._
+import cats.syntax.applicative._
+import cats.syntax.either._
+import cats.syntax.functor._
+import cats.syntax.semigroupk._
+import fs2.text.utf8Decode
 import io.circe.generic.auto._
-import org.http4s.Uri
+import org.http4s.circe.CirceEntityDecoder._
 import org.http4s.client.Client
-import org.http4s.circe._
+import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.implicits._
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.{HttpRoutes, Status, Uri}
+import sttp.model.StatusCode
+import sttp.tapir._
+import sttp.tapir.docs.openapi.OpenAPIDocsInterpreter
 import sttp.tapir.generic.auto._
 import sttp.tapir.json.circe._
-import org.http4s.circe.CirceEntityDecoder._
-import cats.syntax.flatMap._
-import cats.syntax.functor._
-import org.http4s.client.blaze.BlazeClientBuilder
-import org.http4s.implicits.http4sLiteralsSyntax
-import org.http4s.server.blaze.BlazeServerBuilder
-import cats.syntax.either._
+import sttp.tapir.openapi.circe.yaml.RichOpenAPI
+import sttp.tapir.openapi.{Info, OpenAPI}
+import sttp.tapir.server.http4s.Http4sServerInterpreter
+import sttp.tapir.swagger.http4s.SwaggerHttp4s
 
 import scala.concurrent.ExecutionContext.global
 
@@ -24,10 +30,32 @@ object Server extends IOApp {
 
   case class Pokemon(description: String, imageUrl: String)
 
+  val internalError = statusMapping(StatusCode.InternalServerError, jsonBody[InternalError])
+  val notFoundError = statusMapping(StatusCode.NotFound, jsonBody[NotFoundError])
+
   val getPokemonEndpoint = endpoint.get
     .in("pokemon")
     .in(query[String]("name"))
     .out(jsonBody[Pokemon])
+    .errorOut(oneOf[ApiError](internalError, notFoundError))
+
+  val docs: OpenAPI =
+    OpenAPIDocsInterpreter.toOpenAPI(Iterable(getPokemonEndpoint), Info("Pokemon API", "1"))
+  val docsRoute: HttpRoutes[IO] = new SwaggerHttp4s(docs.toYaml).routes
+
+  sealed trait ApiError {
+    def message: String
+  }
+
+  object ApiError {
+    def internalError(message: String): ApiError = InternalError(message)
+
+    def notFoundError(message: String): ApiError = NotFoundError(message)
+  }
+
+  case class InternalError(message: String) extends ApiError
+
+  case class NotFoundError(message: String) extends ApiError
 
 
   // http client
@@ -36,7 +64,7 @@ object Server extends IOApp {
 
   case class Specie(name: String, url: String)
 
-  case class Response(species: Specie, sprites: Sprites)
+  case class PokemonResponse(species: Specie, sprites: Sprites)
 
   case class Language(name: String)
 
@@ -44,35 +72,51 @@ object Server extends IOApp {
 
   case class SpeciesResponse(flavor_text_entries: List[Flavor])
 
-  trait PokemonRetrieve[F[_]] {
-    def retrievePokemonUrl(name: String): F[Pokemon]
+  trait PokemonRetriever[F[_]] {
+    def retrievePokemonUrl(name: String): F[Either[ApiError, Pokemon]]
   }
 
-  object PokemonRetrieve {
-    def apply[F[_] : Sync](client: Client[F], uri: Uri): PokemonRetrieve[F] = new PokemonRetrieve[F] {
-      override def retrievePokemonUrl(name: String): F[Pokemon] = {
-        for {
-          pokemonResponse <- client.expect[Response](uri / "pokemon" / name)
-          description = Uri.unsafeFromString(pokemonResponse.species.url)
-          speciesResponse <- client.expect[SpeciesResponse](description)
-          description = speciesResponse.flavor_text_entries.find(_.language.name == "en").map(_.flavor_text).getOrElse("")
+  object PokemonRetriever {
+
+    def apply[F[_] : Sync](client: Client[F], uri: Uri): PokemonRetriever[F] = new PokemonRetriever[F] {
+      override def retrievePokemonUrl(name: String): F[Either[ApiError, Pokemon]] = {
+        (for {
+          pokemonResponse <- EitherT(client.get(uri / "pokemon" / name) {
+            case Status.Successful(r) =>
+              r.attemptAs[PokemonResponse].leftMap(_.message).leftMap(ApiError.internalError).value
+            case Status.NotFound(_) =>
+              ApiError.notFoundError(s"Pokemon ${name} not found").asLeft[PokemonResponse].pure
+            case failure => failure.body.through(utf8Decode).compile.string.map(ApiError.internalError).map(_.asLeft[PokemonResponse])
+          })
+          speciesUrl <- EitherT(Uri.fromString(pokemonResponse.species.url)
+            .leftMap(failure => InternalError(failure.message)).pure)
+          speciesResponse <- EitherT.right[ApiError](client.expect[SpeciesResponse](speciesUrl))
+          description = speciesResponse
+            .flavor_text_entries
+            .find(_.language.name == "en")
+            .fold("No description")(_.flavor_text.trim)
         } yield Pokemon(
           description = description,
           imageUrl = pokemonResponse.sprites.front_default
-        )
+        )).value
       }
     }
   }
 
 
   override def run(args: List[String]): IO[ExitCode] = {
-    for {
-      pokemon <- BlazeClientBuilder[IO](global).resource.use(client => {
-        val pokemonRetrieve = PokemonRetrieve[IO](client, uri"https://pokeapi.co/api/v2/")
-        pokemonRetrieve.retrievePokemonUrl("pikachu")
+    (for {
+      route <- BlazeClientBuilder[IO](global).stream.map(client => {
+        val pokemonRetriever = PokemonRetriever[IO](client, uri"https://pokeapi.co/api/v2/")
+        Http4sServerInterpreter.toRoutes(getPokemonEndpoint) { pokemonName =>
+          pokemonRetriever.retrievePokemonUrl(pokemonName)
+        }
       })
-      _ <- IO(println(s"Pokemon: ${pokemon}"))
-    } yield ExitCode.Success
+      _ <- BlazeServerBuilder[IO](executionContext)
+        .bindHttp(9090, "localhost")
+        .withHttpApp((route <+> docsRoute).orNotFound)
+        .serve
+    } yield ()).compile.drain.as(ExitCode.Success)
   }
 
 }
